be942ecc12b2df6177beaf19a31d40fd8430123d
mariaek <133098591+mariaek@users.noreply.github.com>
2023-05-10 15:01:21 +0200

    Add files via upload

---
+++ scenario3.m
@@ -0,0 +1,48 @@
+clc
+close all
+clear all
+
+t = [0:200];
+
+%plant, hare, and lynx initial population
+y0 = [0.2 0.6 1];
+%in order to decrease population of lynx we have to increase their number
+%but decrease their source of food 
+
+%succesful predation
+a1 = 5;
+a2  = 0.5;
+%increased the number of successful predation for a1
+
+%unsuccesful predation
+b1 = 1;
+b2 = 5;
+%
+%death rate constants
+d1 = 1;
+d2 = 0.25;
+
+%simulataneously increased the death rate of both hare and lynx
+% initially they were at d1= 0.1 and d=0.01 
+% the death rate of the prey has to be larger than that of the predator
+
+[t,y] = ode45(@(t,y) plants_hare_lynx(t,y,a1,a2,b1,b2,d1,d2), t, y0);
+
+% plotting the population over time
+figure("Name",'Plant-Hare-Lynx')
+plot(t,y)
+title('Plant/Hare/Lynx Populations Over Time')
+xlabel('Time [months]')
+ylabel('Population')
+legend('Plant','Hare','lynx','Location','North')
+
+% plotting the state space
+figure("Name",'State space')
+plot3(y(:,1),y(:,2), y(:,3))
+title('State space')
+xlabel('Plants')
+ylabel('Hare')
+zlabel('lynx')
+
+%the number of hares and plants stays constant while the population of lynx
+%reaches zero after 30 days
